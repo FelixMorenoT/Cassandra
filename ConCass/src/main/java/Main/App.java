af762f1f130bbd85c5dd26ed780f44d74bf60f6c
FelixMorenoT <felixmorenot18@hotmail.com>
2017-11-16 09:18:44 -0500

    Comienzo de codificacion de Algoritmo

--- ConCass/src/main/java/Main/App.java
+++ ConCass/src/main/java/Main/App.java
@@ -6,13 +6,11 @@ import weka.clusterers.SimpleKMeans;
 public class App {
 
 	public static void main(String[] args) {
-		SimpleKMeans kmeans = new SimpleKMeans();
-		kmeans.
 		
 	    Conexion client = new Conexion();
 	    client.connect("127.0.0.1");
-	    //client.createSchema("dbf");
-	    client.consultingAll("dbf","dataforest");
+	    client.createSchema("dbf");
+	    //client.consultingAll("dbf","dataforest");
 	 
 	    //client.deleteKeySpace("db1");
 
