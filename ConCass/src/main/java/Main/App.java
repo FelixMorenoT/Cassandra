8a484933719b948b6e9a062f772f26f267ee6258
FelixMorenoT <felixmorenot18@hotmail.com>
2017-11-16 08:55:29 -0500

    Update 16/Nov

--- ConCass/src/main/java/Main/App.java
+++ ConCass/src/main/java/Main/App.java
@@ -1,16 +1,21 @@
 package Main;
 
 import ConexionCassandra.ConCass.Conexion;
-
+import weka.*;
+import weka.clusterers.SimpleKMeans;
 public class App {
 
 	public static void main(String[] args) {
+		SimpleKMeans kmeans = new SimpleKMeans();
+		kmeans.
+		
 	    Conexion client = new Conexion();
 	    client.connect("127.0.0.1");
 	    //client.createSchema("dbf");
 	    client.consultingAll("dbf","dataforest");
 	 
 	    //client.deleteKeySpace("db1");
+
 	}
 
 }
